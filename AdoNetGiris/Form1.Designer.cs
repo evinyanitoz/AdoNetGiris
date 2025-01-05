ad86a02ad9f4274c7b7cef4099a8b7ea1e9f4633
evinyanitoz <eyanitoz@gmail.com>
2025-01-05 12:27:26 +0300

    Add project files.

---
+++ AdoNetGiris/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace AdoNetGiris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtKatAdi = new System.Windows.Forms.TextBox();
+            this.btnKategoriEkle = new System.Windows.Forms.Button();
+            this.txtAciklama = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResim = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kategori listele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(968, 334);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ürünleri Listele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnUpdate);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.txtResim);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtAciklama);
+            this.groupBox1.Controls.Add(this.btnKategoriEkle);
+            this.groupBox1.Controls.Add(this.txtKatAdi);
+            this.groupBox1.Location = new System.Drawing.Point(12, 401);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 239);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kategori İşlemleri";
+            // 
+            // txtKatAdi
+            // 
+            this.txtKatAdi.Location = new System.Drawing.Point(92, 50);
+            this.txtKatAdi.Name = "txtKatAdi";
+            this.txtKatAdi.Size = new System.Drawing.Size(149, 22);
+            this.txtKatAdi.TabIndex = 0;
+            // 
+            // btnKategoriEkle
+            // 
+            this.btnKategoriEkle.Location = new System.Drawing.Point(42, 135);
+            this.btnKategoriEkle.Name = "btnKategoriEkle";
+            this.btnKategoriEkle.Size = new System.Drawing.Size(199, 26);
+            this.btnKategoriEkle.TabIndex = 1;
+            this.btnKategoriEkle.Text = "Kategori Ekle";
+            this.btnKategoriEkle.UseVisualStyleBackColor = true;
+            this.btnKategoriEkle.Click += new System.EventHandler(this.btnKategoriEkle_Click);
+            // 
+            // txtAciklama
+            // 
+            this.txtAciklama.Location = new System.Drawing.Point(92, 76);
+            this.txtAciklama.Name = "txtAciklama";
+            this.txtAciklama.Size = new System.Drawing.Size(149, 22);
+            this.txtAciklama.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kategori Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Açıklama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resim";
+            // 
+            // txtResim
+            // 
+            this.txtResim.Location = new System.Drawing.Point(92, 107);
+            this.txtResim.Name = "txtResim";
+            this.txtResim.Size = new System.Drawing.Size(149, 22);
+            this.txtResim.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(199, 26);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Kategori Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(42, 199);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(199, 26);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "Kategori  Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 953);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtResim;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAciklama;
+        private System.Windows.Forms.Button btnKategoriEkle;
+        private System.Windows.Forms.TextBox txtKatAdi;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ AdoNetGiris/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetGiris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection(@"Server=LAPTOP-F5PIAQHS\BOTANIKSQL;Initial Catalog=Northwind;Integrated Security=true");
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from Categories", baglan);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from Products", baglan);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+         
+        }
+        void Kategorigetir() {
+
+
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from Categories", baglan);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+
+        }
+        private void btnKategoriEkle_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("Insert into categories(CategoryName,Description) values(@p1,@p2)", baglan);
+            baglan.Open();
+            cmd.Parameters.AddWithValue("@p1", txtKatAdi.Text);
+            cmd.Parameters.AddWithValue("@p2", txtAciklama.Text);
+            cmd.ExecuteNonQuery();
+            baglan.Close();
+            Kategorigetir();
+
+           /* #region INFO
+            cmd.ExecuteNonQuery();//GERİ DEĞER DÖNMEYEN SELECT HARİCİ İŞLEMLER İÇİN
+            cmd.ExecuteReader();//SELECT İŞLEMİNİ SATIR SATIR ALMAK İÇİN
+            cmd.ExecuteScalar();//TEK SATIR TEK SÜTUN SORGULAR İÇİNDİR .1.Cİ SATIR 1.Cİ SÜTUNU DÖNDÜRÜR.
+
+
+
+            #endregion
+           */
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //DataBound item araştır.
+            var id = dataGridView1.CurrentRow.Cells[0].Value;    
+            SqlCommand cmd = new SqlCommand("delete from Categories where CategoryId=@p1", baglan);
+            cmd.Parameters.AddWithValue("@p1", id);
+            baglan.Open();
+           
+            cmd.ExecuteNonQuery();
+            baglan.Close();
+            Kategorigetir();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var id = dataGridView1.CurrentRow.Cells[0].Value;
+            SqlCommand komut=new SqlCommand("Update Categories set CategoryName=@p1 where CategoryId=@p2",baglan);
+            komut.Parameters.AddWithValue("@p1",txtKatAdi.Text);
+            komut.Parameters.AddWithValue("@p2",id);
+            baglan.Open();
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            Kategorigetir();
+        }
+    }
+}
